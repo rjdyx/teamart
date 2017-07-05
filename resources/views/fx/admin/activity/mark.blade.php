6c4ae9ddbeefdc920b56ad64050a03c4b199451c
guosenlin1026 <445931644@qq.com>
2017-07-05 18:40:16 +0800

    [mod]修改积分活动

--- resources/views/fx/admin/activity/mark.blade.php
+++ resources/views/fx/admin/activity/mark.blade.php
@@ -4,130 +4,79 @@
 @section('t1')活动@endsection
 
 @section('css')
-
 @endsection
 
 @section('script')
     @parent
-
 @endsection
 
 @section('content')
-
-
-   
-
-
-
-      <!-- Main content of agentRole-->
-      <section class="content">
-        <div class="row">
-          <!-- 代理商角色列表 -->
-          <div class="col-xs-12">
-            <div class="box box-success">
-              <div class="box-header">
-                <div class="input-group input-group-sm" style="width: 470px;">
-                  <form action="{{url("admin/activity/mark")}}" method="get">
-                    <div class="row">
-                      <div class="col-sm-4">
-                        <select class="form-control input-sm" name="sort">
-                          @if(empty($_GET['sort']))
-                            <option value="10"  >状态</option>
-                            <option value="11"  >有货</option>
-                            <option value="12"  >无货</option>
-                          @else
-                            <option value="10">状态</option>
-
-                            <option value="11"
-                                    @if($_GET['sort'] == 11)
-                                    selected
-                                    @endif
-                            >有货</option>
-                            <option value="12"
-                                    @if($_GET['sort'] == 12)
-                                    selected
-                                    @endif
-                            >无货</option>
-                          @endif
-                        </select>
-                      </div>
-                      <div class="col-sm-8"><input type="text" name="table_search" class="form-control pull-right input-sm" placeholder="请输入搜索内容"></div>
-                    </div>
-                    <div class="input-group-btn">
-                      <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
-                    </div>
-                    </form>
+  <section class="content">
+    <div class="row">
+      <!-- 代理商角色列表 -->
+      <div class="col-xs-12">
+        <div class="box box-success">
+          <div class="box-header">
+            <div class="input-group input-group-sm" style="width:50%;">
+                <div class="row">
+                  <div class="col-sm-4">
+                    <select class="form-control input-sm" name="state" id="searchState">
+                        <option value="" >状态</option>
+                        <option value="1" >有货</option>
+                        <option value="0" >无货</option>
+                    </select>
+                  </div>
+                  <div class="col-sm-8"><input type="text" name="name" class="form-control pull-right input-sm" placeholder="请输入搜索内容" id="searchName"></div>
                 </div>
-                <div class="box-tools">
-                 <a href="{{url("admin/activity/mark/create")}}"> <button type="button" class="btn btn-block btn-success btn-sm" id="addUser">新建积分</button></a>
+                <div class="input-group-btn">
+                  <button type="submit" class="btn btn-default"  onclick="search({{$lists->currentPage()}},['searchName','searchState']);"><i class="fa fa-search"></i></button>
                 </div>
-              </div>
-              <!-- /.box-header -->
-              <div class="box-body table-responsive no-padding">
-
-                <table class="table table-hover">
-                  <tbody><tr>
-                    <th><!-- <button type="button" class="btn btn-default btn-sm checkbox-toggle"><i class="fa fa-square-o"></i></button> --></th>
-                    <th>编号</th>
-                    <th>商品名称</th>
-                    <th>状态</th>
-                    <th>积分使用</th>
-                    <th>库存</th>
-                    <th>描述</th>
-                    <th>当前价格</th>
-                    <th>操作</th>
-                  </tr>
-                  @foreach($lists as $k=>$list)
-                  <tr>
-                    <td><input type="checkbox" class="check" value="{{$list->id}}"></td>
-                    <td>{{$k+1}}</td>
-                    <td>{{$list->name}}</td>
-                    @if($list->state==1)
-                    <td> 有货 </td>
-                    @else
-                      <td> 缺货 </td>
-                    @endif
-                    @if($list->grade==1)
-                      <td> 可使用 </td>
-                    @else
-                      <td> 不可用 </td>
-                    @endif
-                    <td>{{$list->stock}}</td>
-                    <td>{{$list->desc}}</td>
-                    <td>{{$list->price}}</td>
-
-                    <td><div style="color: #dd4b39"><a href="{{url("admin/activity/mark/$list->id/edit")}}"><i class="fa fa-trash-o"  style="margin-right: 5px;cursor: pointer;"></i></a></div>
-                   </td>
-                  </tr>
-                  @endforeach
-
-                  <tr>
-                    <td><button type="button" class="btn btn-default btn-sm checkbox-toggle"><i class="fa fa-square-o">全选</i></td>
-
-                      <td><button type="button" onclick="dels();" class="btn btn-block btn-default btn-sm">删除</button></td>
-
-
-                    <th colspan="9">
-                      @if(isset($_GET['sort'])&&isset($_GET['table_search']))
-                        {{$lists->appends(['sort'=>$_GET['sort'],'table_search'=>$_GET['table_search']])->links()}}
-                      @else
-                        {{$lists->links()}}
-                      @endif
-                      共{{ $lists->lastPage() }}页
-
-                    </th>
-                  </tr>
-                </tbody></table>
-              </div>
-              <!-- /.box-body -->
             </div>
-            <!-- /.box -->
+            <div class="box-tools">
+             <a href="{{url('/admin/activity/mark/create')}}"> <button type="button" class="btn btn-block btn-success btn-sm" id="addUser">添加商品</button></a>
+            </div>
           </div>
-          <!-- /代理商角色列表 -->
+
+          <div class="box-body table-responsive no-padding">
+            <table class="table table-hover">
+              <tbody><tr>
+                <th>多选框</th>
+                <th>编号</th>
+                <th>商品图片</th>
+                <th>商品名称</th>
+                <th>价格</th>
+                <th>状态</th>
+                <th>库存</th>
+                <th>操作</th>
+              </tr>
+              @foreach($lists as $k=>$list)
+              <tr>
+                <td><input type="checkbox" class="check" value="{{$list->id}}"></td>
+                <td>{{$k+1}}</td>
+                <td><img src="{{url('')}}/{{$list->thumb}}" alt="" width="40px" height="35px"></td>
+                <td>{{$list->name}}</td>
+                <td>&yen; {{$list->price}}</td>
+                <td>@if($list->state==1) 有货 @else缺货 @endif </td>
+                <td>{{$list->stock}}</td>
+                <td><div style="color: #dd4b39"><a href="{{url("admin/activity/mark/$list->id/edit")}}"><i class="fa fa-trash-o"  style="margin-right: 5px;cursor: pointer;"></i></a></div>
+               </td>
+              </tr>
+              @endforeach
+
+              <tr>
+                <td><button type="button" class="btn btn-default btn-sm checkbox-toggle"><i class="fa fa-square-o">全选</i></td>
+                  <td><button type="button" onclick="dels();" class="btn btn-block btn-default btn-sm">删除</button></td>
+                <th colspan="12">
+                  <ul class="pagination pagination-sm no-margin pull-right">
+                      {{ $lists->appends(['name' => '','state'=>''])->links() }}
+                      共{{ $lists->lastPage() }}页
+                  </ul>
+                </th>
+              </tr>
+            </tbody></table>
+          </div>
         </div>
-      </section>
-      <!-- /.content -->
-
-
-
+      </div>
+    </div>
+  </section>
 @endsection
