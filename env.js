6a94fd1cebcb8e21f67d2886a922bf474811e2e3
bwx9394 <298057325@qq.com>
2017-07-12 16:39:34 +0800

    [mod]解决冲突

---
+++ env.js
@@ -0,0 +1,8 @@
+module.exports = {
+	port: 8080,
+	app_url: 'http://www.fx.com/',
+	// http://fx.caishi360.com/
+	// http://www.fx.com/
+	isServer: false,
+	isAdmin: false // side == true ? 'admin' : 'fx'
+}
