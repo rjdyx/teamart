4f09283ed7529f43ee4f036374bdfa63c45d4f49
guosenlin1026 <445931644@qq.com>
2017-07-12 17:35:23 +0800

    [mod]git

--- env.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	port: 8080,
-	app_url: 'http://www.fx.com/',
-	// http://fx.caishi360.com/
-	// http://www.fx.com/
-	isServer: false,
-	isAdmin: false // side == true ? 'admin' : 'fx'
-}
