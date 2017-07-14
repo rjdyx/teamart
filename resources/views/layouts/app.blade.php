a9652ac921a924bb0793607a0074f4a6329a8c04
bwx9394 <298057325@qq.com>
2017-07-14 10:29:06 +0800

    [mod]修改路径

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,8 +38,8 @@
     <title>@if(count($data)){{$data->name}} @endif - @yield('title')</title>
 
     <!-- Styles -->
-    {{-- <link rel="stylesheet" href="{{url('/fx/build/css/index.css')}}"> --}}
-    <link rel="stylesheet" href="http://localhost:8080/fx/build/css/index.css">
+    <link rel="stylesheet" href="{{url('/fx/build/css/index.css')}}"> 
+    {{-- <link rel="stylesheet" href="http://localhost:8080/fx/build/css/index.css">--}}
     @yield('css')
 
     <!-- Scripts -->
@@ -52,10 +52,10 @@
 <body class="base-fontsize">
     @yield('content')
     <!-- Scripts -->
-    {{-- <script src="{{url('/fx/build/vendor-bundle.js')}}"></script>
-    <script src="{{url('/fx/build/index.js')}}"></script> --}}
-    <script src="http://localhost:8080/fx/build/vendor-bundle.js"></script>
-    <script src="http://localhost:8080/fx/build/index.js"></script>
+    <script src="{{url('/fx/build/vendor-bundle.js')}}"></script>
+    <script src="{{url('/fx/build/index.js')}}"></script>
+    {{-- <script src="http://localhost:8080/fx/build/vendor-bundle.js"></script>
+    <script src="http://localhost:8080/fx/build/index.js"></script> --}}
     <script>
         var deviceWidth = document.documentElement.clientWidth;
         if(deviceWidth > 640) deviceWidth = 640;
