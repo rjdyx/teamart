14965102f4c85fcd065920e6ba881221dc5bd3ca
bwx9394 <298057325@qq.com>
2017-07-14 17:11:59 +0800

    [mod]修改引入

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,8 +41,8 @@
     <title>@if(count($data)){{$data->name}} @endif - @yield('title')</title>
 
     <!-- Styles -->
-    {{-- <link rel="stylesheet" href="{{url('/fx/build/css/index.css')}}"> --}}
-    <link rel="stylesheet" href="http://localhost:8080/fx/build/css/index.css">
+    <link rel="stylesheet" href="{{url('/fx/build/css/index.css')}}">
+    {{-- <link rel="stylesheet" href="http://localhost:8080/fx/build/css/index.css"> --}}
     @yield('css')
 
     <!-- Scripts -->
