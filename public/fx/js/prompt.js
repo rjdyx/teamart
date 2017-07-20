bc28e2567996eb052073c21a2bb4ca9973cb25ca
bwx9394 <298057325@qq.com>
2017-07-20 18:32:36 +0800

    [mod]修改ios图片上传测试

--- public/fx/js/prompt.js
+++ public/fx/js/prompt.js
@@ -50,3 +50,8 @@ exports.image = (src) => {
 exports.qrcode = () => {
 	$('.prompt').addClass('active').find('#qrcode').removeClass('hide')
 }
+
+exports.test = () => {
+	$('.prompt').addClass('active').find('.prompt_box_content').html(msg).end()
+		.find('.message').removeClass('hide')
+}
