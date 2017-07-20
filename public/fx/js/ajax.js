829458b03566da9fc54c6392d4218f51be879426
bwx9394 <298057325@qq.com>
2017-07-20 18:24:49 +0800

    [mod]显示错误

--- public/fx/js/ajax.js
+++ public/fx/js/ajax.js
@@ -65,6 +65,7 @@ const ajax = (type, url, data = {}, isEdit = false, hasfile = false, errFn = tru
 						prompt.message('请先登录', 'http://' + window.location.host + '/login')
 					} else {
 						// prompt.message('请求错误')
+						alert(err.stack)
 						prompt.message(err.stack)
 					}
 				} else {
