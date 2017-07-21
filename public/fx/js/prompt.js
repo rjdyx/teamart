efeb857328634be1b75f60163d8703318281d5fc
bwx9394 <298057325@qq.com>
2017-07-21 16:25:08 +0800

    [mod]修改显示弹窗

--- public/fx/js/prompt.js
+++ public/fx/js/prompt.js
@@ -1,16 +1,15 @@
 exports.init = () => {
 	$('.J_hide_prompt').on('tap', function () {
-		$('.prompt').removeClass('active').find('.prompt_box_content').html('').end()
-			.find('.question').addClass('hide').end()
-			.find('.message').addClass('hide').end()
-			.find('.image').addClass('hide').end()
-			.find('#qrcode').addClass('hide').end()
-			.find('.prompt_loading').addClass('hide').find('.prompt_loading_text').text('加载中').end()
+		$('.prompt').removeClass('active')
+			.find('.prompt_image, .prompt_qrcode, .prompt_message, .prompt_question, .prompt_loading').removeClass('atc').end()
+			.find('.prompt_message_content, .prompt_question_content').html('').end()
+			.find('.prompt_loading_content').text('加载中').end()
+			.find('.prompt_image').find('img').remove()
 	})
 	// 点击取消时
 	$('.J_prompt_no').on('tap', function () {
-		$('.prompt').removeClass('active').find('.prompt_box_content').html('').end()
-			.find('.question').addClass('hide')
+		$('.prompt').removeClass('active').find('.prompt_question_content').html('').end()
+			.find('.prompt_question').removeClass('atc')
 	})
 	// 点击图片时
 	// $('.J_prompt_img').on('tap', function () {
@@ -20,46 +19,52 @@ exports.init = () => {
 }
 
 exports.message = (msg, url) => {
-	$('.prompt').addClass('active').find('.prompt_box_content').html(msg).end()
-		.find('.message').removeClass('hide')
+	$('.prompt').addClass('active').find('.prompt_message_content').html(msg).end()
+		.find('.prompt_message').addClass('atc')
 	setTimeout(() => {
 		if (url) {
 			url === 'history' ? history.go(-1) : window.location.href = url
 		}
-		$('.prompt').removeClass('active').find('.prompt_box_content').html('')
-			.find('.message').addClass('hide')
+		$('.prompt').removeClass('active').find('.prompt_message_content').html('').end()
+			.find('.prompt_message').removeClass('atc')
 	}, 1000)
 }
 
 // 参数1 信息
 // 参数2 确定后的回调函数
 exports.question = (msg, fn) => {
-	$('.prompt').addClass('active').find('.prompt_box_content').html(msg).end()
-		.find('.question').removeClass('hide')
+	$('.prompt').addClass('active').find('.prompt_question_content').html(msg).end()
+		.find('.prompt_question').addClass('atc')
 	$('.J_prompt_yes').off('tap').on('tap', function () {
 		fn()
-		$('.prompt').removeClass('active').find('.prompt_box_content').html('').end()
-			.find('.question').addClass('hide')
+		$('.prompt').removeClass('active').find('.prompt_question_content').html('').end()
+			.find('.prompt_question').removeClass('atc')
 	})
 }
 // 大图
 exports.image = (src) => {
-	$('.prompt').addClass('active').find('.image').removeClass('hide').attr('src', src)
+	$('.prompt').addClass('active').find('.prompt_image').addClass('atc')
+	let img = new Image()
+	img.src = src
+	img.onload = function () {
+		$('.prompt_image').append(img)
+	}
+	// $('.prompt').find('#p_img').attr('src', src)
 }
 
 // 二维码
 exports.qrcode = () => {
-	$('.prompt').addClass('active').find('#qrcode').removeClass('hide')
+	$('.prompt').addClass('active').find('.prompt_qrcode').addClass('atc')
 }
 
 // 读取
 exports.loading = (text) => {
-	$('.prompt').addClass('active').find('.prompt_loading').removeClass('hide')
+	$('.prompt').addClass('active').find('.prompt_loading').addClass('atc')
 	if (text) {
-		$('.prompt').find('.prompt_loading_text').text(text)
+		$('.prompt').find('.prompt_loading_content').text(text)
 	}
 }
 
 exports.loadingEnd = () => {
-	$('.prompt').removeClass('active').find('.prompt_loading').addClass('hide').find('.prompt_loading_text').text('加载中')
+	$('.prompt').removeClass('active').find('.prompt_loading').removeClass('atc').find('.prompt_loading_content').text('加载中')
 }
--- resources/views/layouts/prompt.blade.php
+++ resources/views/layouts/prompt.blade.php
@@ -1,19 +1,22 @@
 <div class="prompt">
 	<div class="prompt_bg J_hide_prompt"></div>
-	<div class="prompt_box hide message">
-		<div class="prompt_box_content w-100 txt-c"></div>
+	<div class="prompt_message">
+		<div class="prompt_message_content w-100 txt-c p-10"></div>
 	</div>
-	<div class="prompt_box hide question">
-		<div class="prompt_box_content w-100 txt-c"></div>
-		<div class="prompt_box_btns">
-			<a href="javascript:;" class="prompt_box_btn pull-left J_prompt_yes">确定</a>
-			<a href="javascript:;" class="prompt_box_btn pull-left J_prompt_no">取消</a>
+	<div class="prompt_question">
+		<div class="prompt_question_content w-100 txt-c p-10"></div>
+		<div class="prompt_question_btns">
+			<a href="javascript:;" class="block txt-c prompt_box_btn pull-left J_prompt_yes">确定</a>
+			<a href="javascript:;" class="block txt-c prompt_box_btn pull-left J_prompt_no">取消</a>
 		</div>
 	</div>
-	<img class="J_prompt_img hide image w-100 block" src="" alt="">
-	<div id="qrcode" class="prompt_qrcode hide"></div>
-	<div class="prompt_loading txt-c hide">
+	<div class="prompt_loading txt-c p-10">
 		<i class="fa fa-spinner mt-20"></i>
-		<p class="prompt_loading_text fz-16 w-100"></p>
+		<p class="prompt_loading_content fz-16 w-100"></p>
 	</div>
+	<div class="prompt_image w-100">
+		<i class="fa fa-spinner white"></i>
+		<!-- <img id="p_img" class="w-100 h-100" src="" alt=""> -->
+	</div>
+	<div id="qrcode" class="prompt_qrcode"></div>
 </div>
