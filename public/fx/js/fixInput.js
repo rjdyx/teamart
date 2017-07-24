469ef157e14ef5e6f932f0a8bc1c6c626b623bb3
bwx9394 <298057325@qq.com>
2017-07-24 11:52:54 +0800

    [mod]删除测试代码

--- public/fx/js/fixInput.js
+++ public/fx/js/fixInput.js
@@ -2,20 +2,18 @@ $(function () {
     var windowH = window.innerHeight // 窗口高度
     var isAndroid = navigator.userAgent.match(/Android/i)
     if (isAndroid) {
-        alert('android')
-        window.addEventListener('resize', function (e) {
-            alert('resize')
-            var tempH = window.innerHeight
-            if (windowH > tempH) {
-                // 软键盘出来了
-                alert('窗口高度'+windowH)
-                alert('当前高度'+tempH)
-            } else {
-                // 没有软键盘
-            }
-        })
+        // window.addEventListener('resize', function (e) {
+        //     alert('resize')
+        //     var tempH = window.innerHeight
+        //     if (windowH > tempH) {
+        //         // 软键盘出来了
+        //         alert('窗口高度'+windowH)
+        //         alert('当前高度'+tempH)
+        //     } else {
+        //         // 没有软键盘
+        //     }
+        // })
         $('input, textarea').on('focus', function () {
-            alert('focus')
             // var viewTop = $(window).scrollTop(),            // 可视区域顶部
             //     viewBottom = viewTop + window.innerHeight,  // 可视区域底部
             //     elemTop = $(this).offset().top,
@@ -37,15 +35,10 @@ $(function () {
             }
             var elementTop = getElementPosition(this).top, // 元素顶部位置
                 elementBottom = elementTop + $(this).height() // 元素底部位置
-            // $('.login, .register, .reset, .email, .container').css('overflow', 'auto').scrollTop(elementTop)
-            $(this)[0].scrollIntoView()
-            alert(this)
-            alert(elementTop)
-            alert(elementBottom)
-            // $('.login, .register, .reset, .email, .container').css({'position': 'absolute', 'top': elementTop + 'px', 'left': 0})
+            $('.login, .register, .reset, .email, .container').css({'position': 'absolute', 'top': -(elementTop - 10) + 'px', 'left': 0, 'Zindex': 1})
+            // $(this)[0].scrollIntoView() // 无效
         }).on('blur', function () {
-            alert('blur')
-            // $('.login, .register, .reset, .email, .container').removeAttr('style')
+            $('.login, .register, .reset, .email, .container').removeAttr('style')
         })
     }
     // $('input, textarea').on('focus', function () {
