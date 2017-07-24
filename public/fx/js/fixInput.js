e34f21d43741961d4d46d770ad5da24e1fdeee2e
bwx9394 <298057325@qq.com>
2017-07-24 11:43:54 +0800

    [mod]测试安卓键盘遮挡

--- public/fx/js/fixInput.js
+++ public/fx/js/fixInput.js
@@ -2,7 +2,9 @@ $(function () {
     var windowH = window.innerHeight // 窗口高度
     var isAndroid = navigator.userAgent.match(/Android/i)
     if (isAndroid) {
+        alert('android')
         window.addEventListener('resize', function (e) {
+            alert('resize')
             var tempH = window.innerHeight
             if (windowH > tempH) {
                 // 软键盘出来了
@@ -13,6 +15,7 @@ $(function () {
             }
         })
         $('input, textarea').on('focus', function () {
+            alert('focus')
             // var viewTop = $(window).scrollTop(),            // 可视区域顶部
             //     viewBottom = viewTop + window.innerHeight,  // 可视区域底部
             //     elemTop = $(this).offset().top,
@@ -23,21 +26,26 @@ $(function () {
             // console.log(elemHeight)
 
             /* 获得元素的位置信息 */
-            // var getElementPosition = function(elem) {
-            //     var defaultRect = {top: 0, left: 0};
-            //     var rect = (elem.getBoundingClientRect && elem.getBoundingClientRect()) || defaultRect;
-            //     var ret = {
-            //         top: rect.top + document.body.scrollTop,
-            //         left: rect.left + document.body.scrollLeft
-            //     }
-            //     return ret;
-            // }
-            // var elementTop = getElementPosition(this).top, // 元素顶部位置
-            //     elementBottom = elementTop + $(this).height() // 元素底部位置
+            var getElementPosition = function(elem) {
+                var defaultRect = {top: 0, left: 0};
+                var rect = (elem.getBoundingClientRect && elem.getBoundingClientRect()) || defaultRect;
+                var ret = {
+                    top: rect.top + document.body.scrollTop,
+                    left: rect.left + document.body.scrollLeft
+                }
+                return ret;
+            }
+            var elementTop = getElementPosition(this).top, // 元素顶部位置
+                elementBottom = elementTop + $(this).height() // 元素底部位置
             // $('.login, .register, .reset, .email, .container').css('overflow', 'auto').scrollTop(elementTop)
-            this.scrollIntoView()
+            $(this)[0].scrollIntoView()
+            alert(this)
+            alert(elementTop)
+            alert(elementBottom)
+            // $('.login, .register, .reset, .email, .container').css({'position': 'absolute', 'top': elementTop + 'px', 'left': 0})
         }).on('blur', function () {
-            // $('.login, .register, .reset, .email, .container').removeAttr('style' )
+            alert('blur')
+            // $('.login, .register, .reset, .email, .container').removeAttr('style')
         })
     }
     // $('input, textarea').on('focus', function () {
