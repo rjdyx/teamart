676d30afbb33addc857d2a245421e97f8697a061
guosenlin1026 <445931644@qq.com>
2017-08-01 15:38:46 +0800

    支付

--- app/Http/Controllers/Home/OrderPayController.php
+++ app/Http/Controllers/Home/OrderPayController.php
@@ -75,33 +75,22 @@ class OrderPayController extends Controller
 
 		$arr["appId"] = $this::APPID;//微信公众号id
 		$arr["nonceStr"] = $rand;
-		$arr["timeStamp"] = $time;
-		// $arr["signType"] = "MD5";
-		$arr["partnerId"] = $this::MCHID;//商户号
-		$arr["prepayId"] = $res["prepay_id"];//预支付id
-		// $arr["package"] = "prepay_id=".$res["prepay_id"];//订单详情扩展字符串	
-		$arr["package"] = "Sign=WXPay";//扩展字段
+		$arr["timeStamp"] = "$time";
+		$arr["signType"] = "MD5";
+		// $arr["partnerId"] = $this::MCHID;//商户号
+		// $arr["prepayId"] = $res["prepay_id"];//预支付id
+		$arr["package"] = "prepay_id=".$res["prepay_id"];//订单详情扩展字符串	
+		// $arr["package"] = "Sign=WXPay";//扩展字段
 		$arr["paySign"] = $this->getSign();//生成签名
 
-		$arr["appid"] = $this::APPID;//微信公众号id
-		$arr["noncestr"] = $rand;
-		$arr["timestamp"] = $time;
-		$arr["partnerid"] = $this::MCHID;//商户号	
-		$arr["prepayid"] = $res["prepay_id"];//预支付id
-		$arr["sign"] = $arr["paySign"];//生成签名
-		$arr["total_fee"] = 1;//金额
+		// $arr["appid"] = $this::APPID;//微信公众号id
+		// $arr["noncestr"] = $rand;
+		// $arr["timestamp"] = "$time";
+		// $arr["partnerid"] = $this::MCHID;//商户号	
+		// $arr["prepayid"] = $res["prepay_id"];//预支付id
+		// $arr["sign"] = $arr["paySign"];//生成签名
 
 		return json_encode($arr);
-
-		// $arr['appid'] = $this::APPID;//商户号
-		// $arr['partnerid'] = $this::MCHID;//商户号
-		// $arr['prepayid'] = $res['prepay_id'];//预支付id
-		// $arr['package'] = 'Sign=WXPay';//扩展字段
-		// $arr['noncestr'] = $rand;//随机字符串
-		// $arr['timestamp'] = $time;//当前时间戳
-		// $arr['sign'] = $sign;//生成签名	
-		// $arr['sign'] = $this->getSign($arr);//生成签名	
-		// return json_encode($arr);
 	}
 
 	//产生随机字符串，不长于32位
