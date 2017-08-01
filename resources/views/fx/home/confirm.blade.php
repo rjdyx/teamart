d0edf4bc02b0e2fe58b115d41296939bb263f10b
guosenlin1026 <445931644@qq.com>
2017-08-01 15:25:05 +0800

    支付

--- resources/views/fx/home/confirm.blade.php
+++ resources/views/fx/home/confirm.blade.php
@@ -27,15 +27,16 @@
 		function jsApiCall(data)
 		{
        		console.log(data)
-			WeixinJSBridge.invoke('getBrandWCPayRequest',
-			{
-                "appId": data.appId, //公众号名称，由商户传入
-                "timeStamp": data.timeStamp, //时间戳
-                "nonceStr": data.nonceStr, //随机串
-                "package":  data.package, //扩展包
-                "signType": "MD5", //签名方式
-                "paySign":  data.paySign//微信签名
-			},function(res){
+			WeixinJSBridge.invoke('getBrandWCPayRequest', data
+			// {
+                // "appId": data.appId, //公众号名称，由商户传入
+                // "timeStamp": data.timeStamp, //时间戳
+                // "nonceStr": data.nonceStr, //随机串
+                // "package":  data.package, //扩展包
+                // "signType": "MD5", //签名方式
+                // "paySign":  data.paySign//微信签名
+			// }
+			,function(res){
 				WeixinJSBridge.log(res.err_msg);
 				alert(res.err_code + res.err_desc + res.err_msg);
 			});
