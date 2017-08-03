13a45b34c5a168ebfb7c1652f87a9487fdceecd6
guosenlin1026 <445931644@qq.com>
2017-08-03 15:34:55 +0800

    微信

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,8 +14,8 @@ class HomeController extends Controller
     public function isWeixin() { 
         if (strpos($_SERVER['HTTP_USER_AGENT'], 'MicroMessenger') !== false) { 
             $res = IQuery::GetwxInfo();
-            // echo "<pre>"; var_dump($res);die;
-            $url = 'https://api.weixin.qq.com/sns/userinfo?access_token='.$res['access_token'].'&openid='.$res['openid'].'&lang=zh_CN';
+            // $url = 'https://api.weixin.qq.com/sns/userinfo?access_token='.$res['access_token'].'&openid='.$res['openid'].'&lang=zh_CN';
+            $url = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=".$res['access_token'].'&openid='.$res['openid'].'&lang=zh_CN';
             $info = $this->getJson($url);
             // $data['name'] = $info->nickname;
             // $data['image'] = $info->headimgurl;
