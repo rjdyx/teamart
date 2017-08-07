ce20cd5fae50a81bde9294f43057d08df8faf906
bwx9394 <298057325@qq.com>
2017-08-07 12:06:29 +0800

    [mod]修改后台

--- public/admin/index.js
+++ public/admin/index.js
@@ -1,8 +1,8 @@
 require('expose-loader?$!jquery')
 require('expose-loader?axios!axios')
-require('expose-loader?_!lodash')
+// require('expose-loader?_!lodash')
 
-require('babel-polyfill')
+// require('babel-polyfill')
 
 require('bootstrap/dist/css/bootstrap.css')
 require('font-awesome/css/font-awesome.css')
@@ -25,15 +25,15 @@ require('./css/index.scss')
 require('expose-loader?_valid!./js/validate.js')
 require('expose-loader?datepicker!./js/datepicker.js')
 
-let init = require('./js/index.js')
-$(function () {
-	init.nav()
-	init.adduserClick()
-	init.checkboxToggle()
-	$('.J_FloatNum').on('blur', function () {
-		$(this).val(parseFloat($(this).val()).toFixed(2))
-	})
-	$('.J_IntNum').on('blur', function () {
-		$(this).val(parseFloat($(this).val()).toFixed(0))
-	})
-})
+// let init = require('./js/index.js')
+// $(function () {
+// 	init.nav()
+// 	init.adduserClick()
+// 	init.checkboxToggle()
+// 	$('.J_FloatNum').on('blur', function () {
+// 		$(this).val(parseFloat($(this).val()).toFixed(2))
+// 	})
+// 	$('.J_IntNum').on('blur', function () {
+// 		$(this).val(parseFloat($(this).val()).toFixed(0))
+// 	})
+// })
