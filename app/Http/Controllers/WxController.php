69dfa619ce642f08cce121b7d7d0f4ea89bff502
guosenlin1026 <445931644@qq.com>
2017-08-11 17:28:29 +0800

    绑定

--- app/Http/Controllers/WxController.php
+++ app/Http/Controllers/WxController.php
@@ -27,7 +27,6 @@ class WxController extends Controller
         if (!is_int($pid) && !empty($pid)){
             $pid = explode('pid', $pid)[0];
         }
-        return $pid;
         return view(config('app.theme').'.home.bind_wx')->with(['parter_id'=>$pid]);
     }
 
@@ -140,7 +139,7 @@ class WxController extends Controller
     {
 		if ($this->guard()->attempt($data)) {
 			$user = User::find($id);
-			$user->parter_id = $request->parter_id;
+			$user->pid = $request->parter_id;
 			$wx = session('wx'); // 获取微信用户信息
 			$user->openid = $wx['openid'];
 			if ($user->save()) {
